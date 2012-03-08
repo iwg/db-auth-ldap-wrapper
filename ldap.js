aaab188d270ee1d6c5afa91de020c65b8102cf2f
Xiao Jia <stfairy@gmail.com>
2012-03-08 22:30:45 +0800

    first commit

---
+++ ldap.js
@@ -0,0 +1,37 @@
+// Usage:
+// $ ldapsearch -H ldap://localhost:1389 -x -D cn=username,ou=users -w password -b "o=myhost" objectclass=*
+//                                                         ^^^^^^^^
+
+// Authenticate according to the username/password provided
+// @return true if the credentials are correct
+// @return false if the authentication is failed
+function authenticate(username, password) {
+  // this is just an example which allows any pair of 
+  // username/password which are the same
+  // TODO connect with our own user database for authentication
+  return username === password;
+}
+
+var ldap = require('ldapjs');
+
+var server = ldap.createServer();
+
+server.bind('ou=users', function(req, res, next) {
+  // bind operation guarantees at least there is a pair 'ou=users'
+  // which is { ou: 'users' } in the following shifting
+  // we expect the first pair to be something like { cn: 'username' }
+  var first_pair = req.dn.shift();
+  
+  if (!first_pair.cn)
+    return next(new ldap.InvalidCredentialsError());
+  
+  if (!authenticate(first_pair.cn, req.credentials))
+    return next(new ldap.InvalidCredentialsError());
+  
+  res.end();
+  return next();
+});
+
+server.listen(1389, function() {
+  console.log('LDAP server listening at %s', server.url);
+});
