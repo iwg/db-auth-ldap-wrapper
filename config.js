bcc41acb7ca8e71186a93f7c0c4a1c505008f8b5
Xiao Jia <stfairy@gmail.com>
2012-03-14 15:56:26 +0800

    refactor code

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
-exports.userDatabase="users";//user表单数据库名
-exports.userTableName="users";//user表单名
-exports.dbUser="root";//数据库用户名
-exports.dbPass="";//数据库密码
-exports.dbHost="127.0.0.1";//数据库地址
-exports.loginTrialLimit=30;//错误次数限制
-exports.loginTrialTimeout=600000;//时间限制
+// 用户数据库名
+exports.userDatabase = "users";
+// 用户数据表名
+exports.userTableName = "users";
+
+// 数据库用户名
+exports.dbUser = "root";
+// 数据库密码
+exports.dbPass = "";
+// 数据库地址
+exports.dbHost = "127.0.0.1";
+
+// 登录错误次数限制
+exports.loginTrialLimit = 30;
+// 登录错误超时限制
+exports.loginTrialTimeout = 600000;
